aa71a1b53b393739f36eef26a021eaca4d9a003e
DavidSorge <david.c.sorge@gmail.com>
2019-05-24 00:13:22 -0400

    Implemented move instead of copy, exception handling, implemented scandir.

--- TOI_Sorter.py
+++ TOI_Sorter.py
@@ -30,9 +30,8 @@ Overall specifications:
 from time import time
 import re
 import os
-from shutil import copy
+from shutil import move
 import string
-from winsound import Beep
 
 #-------------------------------------------------------------------------------
 # 1-4: Gets necessary metadata from xml file
@@ -47,43 +46,53 @@ def metadata_getter(xml_file_path):
 
     # Extract Metadata
     file_metadata = {}
+    
     # create a re.compile object that grabs the contents of RecordTitle, NumericPubDate and ObjectType tags.
     d = re.compile(r'<RecordTitle>(.*)</RecordTitle>|<NumericPubDate>(.*)</NumericPubDate>|<ObjectType>([^<]*)</ObjectType>*')
+    
     # creates a list of tuples containing the intended tag strings
     metadata_matrix = d.findall(raw_xml)    
 
     # Assign entries to dictionary
-    numeric_date = metadata_matrix[1][1]
-    file_metadata["year"] = numeric_date[0:4]
-    file_metadata["month"] = numeric_date[4:6]
-    file_metadata["day"] = numeric_date[6:]
-    file_metadata["headline"] = metadata_matrix[0][0][0:30]
-    file_metadata["category"] = metadata_matrix[-1][-1]
-
-    return file_metadata
+    try:
+        numeric_date = metadata_matrix[1][1]
+        file_metadata["year"] = numeric_date[0:4]
+        file_metadata["month"] = numeric_date[4:6]
+        file_metadata["day"] = numeric_date[6:]
+        file_metadata["headline"] = metadata_matrix[0][0][0:30]
+        file_metadata["category"] = metadata_matrix[-1][-1]
+        
+        return file_metadata
+    
+    except:
+        print("Empty file detected:", xml_file_path)
+        pass
+        
 
 #-------------------------------------------------------------------------------
 # 5-7: Uses the metadata to file the xml file in a new data structure
 #-------------------------------------------------------------------------------
 
-def make_nested_directory(file_metadata):
+def make_nested_directory(file_metadata, output_directory):
     """
     Unless it already exists, creates directory
     \toi_archive_sorted\year\month\day\classification
     and returns the path of that directory.
     """
-
-    file_metadata["category"] = file_metadata["category"].lower().translate(str.maketrans('','', string.punctuation))
-    path_structure = ["year", "month", "day", "category"]
-    directory = r"..\..\toi_archive_sorted"
-
-    for layer in path_structure:
-        directory = os.path.join(directory, file_metadata[layer])
-        os.makedirs(directory, exist_ok=True)
-
-    return directory
-
-def sort_file(xml_file):
+    try:
+        file_metadata["category"] = file_metadata["category"].lower().translate(str.maketrans('','', string.punctuation))
+        path_structure = ["year", "month", "day", "category"]
+        directory = output_directory
+    
+        for layer in path_structure:
+            directory = os.path.join(directory, file_metadata[layer])
+            os.makedirs(directory, exist_ok=True)
+    
+        return directory
+    except:
+        pass
+    
+def sort_file(xml_file_path, output_directory):
     """
     Saves a copy of an xml file in directory
     \toi_archive_sorted\year\month\day\classification
@@ -91,108 +100,63 @@ def sort_file(xml_file):
     """
     
     # Call earlier functions
-    file_metadata = metadata_getter(xml_file)
-    new_directory = make_nested_directory(file_metadata)
+    file_metadata = metadata_getter(xml_file_path)
+    new_directory = make_nested_directory(file_metadata, output_directory)
     
     # Construct Windows-compatible file name
-    file_name = file_metadata["headline"].lower()
-    file_name = file_name.translate(str.maketrans('','', string.punctuation))
-    file_name = file_name.replace(" ","_")
-    disalloweds = ["con", "prn", "aux", "nul", "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9", "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9"]
-    if file_name in disalloweds:
-        file_name = file_name + "1"
-    else:
+    try: 
+        file_name = file_metadata["headline"].lower()
+        file_name = file_name.translate(str.maketrans('','', string.punctuation))
+        file_name = file_name.replace(" ","_")
+        disalloweds = ["con", "prn", "aux", "nul", "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9", "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9"]
+        if file_name in disalloweds:
+            file_name = file_name + "1"
+        else:
+            pass
+        file_name = file_name + ".xml"
+        
+        # Move the files to their new location
+        move(xml_file_path, os.path.join(new_directory, file_name))
+    except:
         pass
-    file_name = file_name + ".xml"
-    copy(xml_file, os.path.join(new_directory, file_name))
-
+    
 #-------------------------------------------------------------------------------
 # 8: Applies the code above for all xmls in CWD's containing folder.
 #-------------------------------------------------------------------------------
 
-
-
-def get_folder_list():
-    """
-    Returns a list of the relative paths for all .xml files
-    in relevant directory
-    """
-    
-    return os.listdir(r'..\XML\18380101_20081231')
-    
-def list_xmls_in_folder(folder):
-    """
-    Returns a list of all the files in a folder
-    """
-    
-    print("Now processing", folder)
-    folder_path = os.path.join(r'..\XML\18380101_20081231', folder)
-    file_list = os.listdir(folder_path)
-    return file_list
-
-def sort_all_xmls():
+def sort_all_xmls(input_directory, output_directory):
     """Sorts all xmls in the archive"""
 
     # Start timer and counter
     start_time = time()
     counter = 0
 
-    # Sort files
-    for folder in get_folder_list():
-        files = list_xmls_in_folder(folder)
-        for file in files:   
-            counter += 1
-            file = os.path.join(r'..\XML\18380101_20081231', folder, file)
-            sort_file(file)
+    # Create a generator object for stuff in the input folder.
+    files = os.scandir(input_directory)
+    for file in files:   
+        
+        # Give me a running performance update
+        if counter % 100 == 0:
+            print(counter, "files processed...")
+        else:
+            pass
+        
+        # Generate the next file in the directory and apply the sort function
+        counter += 1
+        file_path = file.path
+        sort_file(file_path, output_directory)
+
+
     
     # Give performance statistics and beep when done.
     elapsed = time() - start_time
     print(f"Sorted {counter} files in {elapsed} seconds")
-    frequency = 2500
-    duration = 500
-    Beep(frequency, duration)
-    
+
 #-------------------------------------------------------------------------------
-# 9: Checks whether all raw files correspond to one and only one sorted file
+# 10: Globals and Calls
 #-------------------------------------------------------------------------------
 
-
-def compare(raw_path, sorted_path):
-    """
-    Check whether there are the same number of xml files in
-    raw folder as there are in sorted folder.
-    """
-    # Start timer
-    lap_time = time()
-    
-    # Generate Walk objects for the two directories
-    raw_walk = os.walk(raw_path)
-    sorted_walk = os.walk(sorted_path)
-
-    # Generate a count of the number of files in each walk object
-    raw_files = sum([len(files) for r, d, files in raw_walk])
-    sorted_files = sum([len(files) for r, d, files in sorted_walk])
-
-    # Tell user whether the number of files are the same or not
-    print(f"there are {raw_files} files in the raw folder \n and {sorted_files} in the sorted folder")
-    if raw_files == sorted_files:
-        print("It worked!")
-    else:
-        print("Something went wrong; time to hunt for bugs!")
-
-    # Give time to complete and beep when done.
-    elapsed = time() - lap_time
-    print(f"The test took {elapsed} seconds")
-    frequency = 2500
-    duration = 500
-    Beep(frequency, duration)
-
-sort_all_xmls()
-
-"""
-# Test code: Only run after you think all files have been sorted.
-
-raw_path = r'..\XML'
-sorted_path = r"..\..\toi_archive_sorted"
-compare(raw_path, sorted_path)
-"""
+if __name__ == '__main__':
+    input_directory = r'C:\Users\David\Documents\Research\toi-archive-subsample\XML'
+    output_directory = r'C:\Users\David\Documents\Research\toi-archive-subsample\test2'
+    sort_all_xmls(input_directory, output_directory)
